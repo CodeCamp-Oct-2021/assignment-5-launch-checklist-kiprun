fdcaa46347bd252369d1198798e63de25fc3159d
R L <kiprun@gmail.com>
2021-11-16 11:56:33 -0600

    New updates

--- script.js
+++ script.js
@@ -2,30 +2,30 @@
 
 window.addEventListener("load", function() {
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+//    let listedPlanets;
+//    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+//    let listedPlanetsResponse;
+//    listedPlanetsResponse.then(function (result) {
+//        listedPlanets = result;
+//        console.log(listedPlanets);
+//    }).then(function () {
+//        console.log(listedPlanets);
+//        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+//    })
    let list = document.getElementById("faultyItems");
    list.style.visibility = "hidden"
    let form = document.querySelector("form");
 
 
    form.addEventListener("submit", function(event){ 
-        let pilot = document.querySelector("input[name=pilot]");
-        let copilot = document.querySelector("input[name=copilot]");
+        let pilot = document.querySelector("input[name=pilotName]");
+        let copilot = document.querySelector("input[name=copilotName]");
         let fuelLevel = document.querySelector("input[name=fuelLevel]");
-        let cargoLevel = document.querySelector("input[name=cargoLevel]");
+        let cargoLevel = document.querySelector("input[name=cargoMass]");
         event.preventDefault();
     
-    formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel)
-
+    formSubmission(document, list, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value)
+        
 
    })
 })
--- scriptHelper.js
+++ scriptHelper.js
@@ -19,28 +19,50 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 function validateInput(testInput) {
     
     if (testInput === ''){
-        return ("Empty");  
+        return "Empty";  
     }
-    else if (!isNaN(testInput)){
-        return("Is a Number");
+    else if (!isNaN(Number(testInput))){
+        return "Is a Number";
     }
      else{
-        return("Is not a Number");
+        return "Is not a Number";
     }
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   if (validateInput(pilot.value) === 'Empty' && validateInput(pilot.value) === "Is a Number"){
-        alert("Please enter a valid name");
+function formSubmission(doc, list, pilot, copilot, fuelLevel, cargoLevel) {
+   if (validateInput(pilot) === 'Empty' || 
+       validateInput(copilot) === "Empty" ||
+       validateInput(fuelLevel) === "Empty" ||
+       validateInput(cargoLevel) === "Empty") {
+            alert("All fields must be filled out");
    }
-   if (validateInput(copilot.value) === 'Empty' && validateInput(pilot.value) === "Is a Number"){
-       alert("Please enter a valid name");
+   if (validateInput(pilot) === 'Is a Number' ||
+       validateInput(copilot) === "Is a Number"){
+            alert("Pilot and coilot names cannot be a number");
    }
-   if (Number(validateInput(fuelLevel.value) <= 10000)){
-       alert("Is Not a Number")
-       document.getElementById("faultyItems").style.visiblity ="visible"
+   if (validateInput(fuelLevel) === 'Is not a Number' ||
+       validateInput(cargoLevel) === 'Is not a Number'){
+            alert("Fuel and cargo levels must be numerical");
    }
    
+   let pilotStatus = document.getElementId("pilotStatus");
+   pilotStatus.innerHTML = "Me"
+   let copilotStatus = document.getElementById("copilotStatus");
+   let fuelStatus = document.getElementId("fuelStatus");
+   let cargoStatus = document.getElementId("cargoStatus");
+   let launchStatus = document.getElementById("launchStatus");
+
+    list.style.visibility = "visible"
+
+//    if (Number(fuelLevel) < 10000)){
+//     list.style.visibility = "visible"
+    
+    
+//    }
+   
+//    if (Number(validateInput(cargoLevel) >10000)){
+//     list.style.visibility = "visible"
+//    }
 
 
 }
